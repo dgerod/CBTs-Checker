353d99ab545445f40e4611c20d0d2b842d02fe30
Diego Escudero <dgerod@xyz-lab.org.es>
2022-03-15 16:14:35 +0100

    Add simple logger.

---
+++ include/simple_logger.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+
+namespace cbt { namespace utils {
+
+class simple_logger
+{
+public:
+    enum class level { NONE = 0, ERROR = 1, WARNING = 5, DEBUG = 10 };
+    static level enabled_level_;
+
+    simple_logger(level current_level);
+
+    template<typename T>
+    simple_logger& operator<<(const T &t)
+    {
+        if(output_)
+        {
+            std::cout << t;
+            return *this;
+        }    
+
+        return *this;
+    }
+  
+    simple_logger& operator<<(std::ostream &(*f)(std::ostream &)) 
+    {
+        if(output_)
+        {
+            std::cout << f;
+            return *this;
+        }  
+    
+        return *this;
+    }
+
+private:
+    bool output_;
+};
+
+}}
---
+++ src/simple_logger.cpp
@@ -0,0 +1,11 @@
+#include "simple_logger.hpp"
+
+
+using namespace cbt::utils;
+
+
+simple_logger::level simple_logger::enabled_level_ = simple_logger::level::NONE;
+
+simple_logger::simple_logger(simple_logger::level current_level) 
+    : output_(current_level <= enabled_level_) 
+{}

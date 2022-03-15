a8d1d05aaf8f4d1d785ec374aed7e0c666250a0f
Diego Escudero <dgerod@xyz-lab.org.es>
2022-03-15 16:18:31 +0100

    Reformat classes and add simple logger calls.

--- src/action.cpp
+++ src/action.cpp
@@ -8,51 +8,37 @@
 
 #include "action.hpp"
 
-namespace cbt{
-    
-    
-    
-    action::action(){
-        label_= "";
-        pre_ = std::vector<std::string>();
-        post_ = std::vector<std::string>();
-    }
-    
 
-    
-    std::string action::get_label() const{
-        return this->label_;
-    };
-    
-    
-    
-    const std::vector<std::string>& action::get_pre() const{
-        return (this->pre_);
-    };
-    
-    
-    
-    const std::vector<std::string>& action::get_post() const{
-        return this->post_;
-    };
-    
-    
-    
-    void action::set_label(std::string new_label){
-        this->label_ = new_label;
-    };
-    
-    
-    
-    void action::insert_pre(std::string new_pre){
-        (this->pre_).push_back(new_pre);
-    };
-    
-    
-    
-    void action::insert_post(std::string new_post){
-        this->post_.push_back(new_post);
-    };
-    
-    
-}//namespace cbt
+using namespace cbt;
+
+
+action::action(){
+    label_= "";
+    pre_ = std::vector<std::string>();
+    post_ = std::vector<std::string>();
+}
+
+std::string action::get_label() const{
+    return this->label_;
+};
+
+const std::vector<std::string>& action::get_pre() const{
+    return (this->pre_);
+};
+
+const std::vector<std::string>& action::get_post() const{
+    return this->post_;
+};
+
+void action::set_label(std::string new_label){
+    this->label_ = new_label;
+};
+
+void action::insert_pre(std::string new_pre){
+    (this->pre_).push_back(new_pre);
+};
+
+void action::insert_post(std::string new_post){
+    this->post_.push_back(new_post);
+};
+

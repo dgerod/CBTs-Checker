3fa72ad06dc30d5cd639430eefc8832c88c73514
Diego Escudero <dgerod@xyz-lab.org.es>
2022-03-16 18:27:34 +0100

    Prepare to load limboole path from a configuration file.

--- cbtc/src/main.cpp
+++ cbtc/src/main.cpp
@@ -12,12 +12,15 @@
 #include "simple_logger.hpp"
 
 
-const std::string LIMBOOLE_PATH = "../dependencies/limboole/limboole";
+const std::string read_configuration(const std::string file_path)
+{
+    std::string limboole_path = "../dependencies/limboole/limboole";
+    return limboole_path;
+}
 
+void execute_limboole(const std::string limboole_path, const std::string output_path_bt_plans, const std::string output_path_gen_req){
 
-void execute_limboole(const std::string output_path_bt_plans, const std::string output_path_gen_req){
-
-    std::string command = LIMBOOLE_PATH + " -s " + output_path_bt_plans + ">> " + output_path_gen_req;
+    std::string command = limboole_path + " -s " + output_path_bt_plans + ">> " + output_path_gen_req;
 
     std::array<char, 128> buffer;
     std::string result;
@@ -31,13 +34,13 @@ void execute_limboole(const std::string output_path_bt_plans, const std::string 
     pclose(pipe);    
 }
 
-void validate_cbt_plan(std::string const output_folder) 
+void validate_cbt_plan(const std::string limboole_path, std::string const output_folder) 
 {
     std::string output_path_bt_plans = output_folder + cbt::name_path_bt_plans;
     std::string output_path_gen_req = output_folder + cbt::name_path_gen_req;
     
     remove(output_path_gen_req.c_str());
-    execute_limboole(output_path_bt_plans, output_path_gen_req);
+    execute_limboole(limboole_path, output_path_bt_plans, output_path_gen_req);
     
     std::ifstream general_requirements;     
     general_requirements.open(output_path_gen_req);        
@@ -47,7 +50,7 @@ void validate_cbt_plan(std::string const output_folder)
 int main(int argc, const char * argv[]) 
 {
     cbt::utils::simple_logger::enabled_level_ = cbt::utils::simple_logger::level::DEBUG;
-
+    
     if (argc != 4)
     {
         std::cerr << "Input Error: expected 4 inputs to the program\n";
@@ -61,19 +64,20 @@ int main(int argc, const char * argv[])
     std::string input_path = argv[1];
     std::string input_req = argv[2]; 
     std::string output_folder = argv[3];
+    std::string limboole_path = read_configuration("config.txt");
     
     std::cout << "Parameters" << std::endl;
     std::cout << "  - tree: " << input_path << std::endl;
     std::cout << "  - initial state: " << input_req << std::endl;
     std::cout << "  - output directory: " << output_folder << std::endl;
-    std::cout << "  - limboole path: " << LIMBOOLE_PATH << std::endl;
+    std::cout << "  - limboole path: " << limboole_path << std::endl;
     std::cout << std::endl;
 
     try
     {
         cbt::conditioned_behavior_tree cbtree;
         cbtree.compute_initial_requirements(input_path, input_req, output_folder);
-        validate_cbt_plan(output_folder);
+        validate_cbt_plan(limboole_path, output_folder);
     }
     catch(const std::exception &e)
     {

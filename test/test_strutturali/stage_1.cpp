0d6e047ff1cd98bf9e9acbbf30a8315d233f1a53
Diego Escudero <dgerod@xyz-lab.org.es>
2022-03-20 12:56:36 +0100

    Fix stage 1 tests.

--- test/test_strutturali/stage_1.cpp
+++ test/test_strutturali/stage_1.cpp
@@ -17,34 +17,40 @@ SCENARIO("Test of the first stage of the pipeline: read file"){
     // Check the branch given by file not found
     WHEN("The XML file is not found"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "not_existing_path"), "Exception occurred while opening the file: input xml file not found.");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "not_existing_path"), 
+            "Exception occurred while opening the file: input xml file not found.");
     }
     
     // If it manages it starts to read the actions
     // We have to check all the branches given by errors in the XML file
     WHEN("The XML file contains a bad formed action"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_no_ActionTemplate.xml"), "Input XML file bad format: object ActionTemplate must contain object YARPAction");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_no_ActionTemplate.xml"), 
+            "Input XML file bad format: object ActionTemplate must contain object YARPAction");
     }
 
     WHEN("The XML file contains actions with contrastant preconditions"){
         cbtc::conditioned_behavior_tree cbtree;            
-         REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_contrastant_preconditions0.xml"), "Input XML file bad format: action cannot have 'c' and 'not c' as pre-conditions");
+         REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_contrastant_preconditions0.xml"), 
+            "Input XML file bad format: action cannot have 'c' and 'not c' as pre-conditions");
     }
 
     WHEN("The XML file contains actions with contrastant preconditions"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_contrastant_preconditions1.xml"), "Input XML file bad format: action cannot have 'c' and 'not c' as pre-conditions");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_contrastant_preconditions1.xml"), 
+            "Input XML file bad format: action cannot have 'c' and 'not c' as pre-conditions");
     }
 
     WHEN("The XML file contains actions with contrastant postconditions"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_contrastant_postconditions0.xml"), "Input XML file bad format: action cannot have 'c' and 'not c' as post-conditions");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_contrastant_postconditions0.xml"), 
+            "Input XML file bad format: action cannot have 'c' and 'not c' as post-conditions");
     }
 
     WHEN("The XML file contains actions with contrastant postconditions"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_contrastant_postconditions1.xml"), "Input XML file bad format: action cannot have 'c' and 'not c' as post-conditions");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_contrastant_postconditions1.xml"), 
+            "Input XML file bad format: action cannot have 'c' and 'not c' as post-conditions");
     }
     
     // Once completed the actions read_file starts to read the BT
@@ -52,50 +58,58 @@ SCENARIO("Test of the first stage of the pipeline: read file"){
     // CBT beginning
     WHEN("The XML file contains a BT not starting with the root"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_no_root.xml"), "Input XML file bad format: Behavior Tree must have a root");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_no_root.xml"), 
+            "Input XML file bad format: Behavior Tree must have a root");
     }
 
     WHEN("The XML file contains a BT without the tag <BehaviorTree> following the tag <root>"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_no_BehaviorTree.xml"), "Input XML file bad format: root must be followed by <BehaviorTree>");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_no_BehaviorTree.xml"), 
+            "Input XML file bad format: root must be followed by <BehaviorTree>");
     }
 
     // CBT internal
     WHEN("The XML file contains a BT with a not recognized CFN"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_not_recognized_CFN.xml"), "Input XML file bad format: Behavior Tree bad format");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_not_recognized_CFN.xml"), 
+            "Input XML file bad format: Behavior Tree bad format");
     }
 
     WHEN("The XML file contains a BT with a not closed sequence tag"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_not_recognized_CFN.xml"), "Input XML file bad format: Behavior Tree bad format");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_not_recognized_CFN.xml"), 
+            "Input XML file bad format: Behavior Tree bad format");
     }
 
     WHEN("The XML file contains a BT with a not closed fallback tag"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_not_recognized_CFN.xml"), "Input XML file bad format: Behavior Tree bad format");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_not_recognized_CFN.xml"), 
+            "Input XML file bad format: Behavior Tree bad format");
     }
 
     WHEN("The XML file contains a BT with a not closed parallel tag"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_not_recognized_CFN.xml"), "Input XML file bad format: Behavior Tree bad format");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_not_recognized_CFN.xml"), 
+        "Input XML file bad format: Behavior Tree bad format");
     }
 
     // CBT ending
     WHEN("The XML file contains a BT in which the tag <Behavior Tree> is not closed"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_no_closed_BehaviorTree.xml"), "Input XML file bad format: object <Behavior Tree> must be closed");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_no_closed_BehaviorTree.xml"), 
+            "Input XML file bad format: object <Behavior Tree> must be closed");
     }
 
     WHEN("The XML file contains a BT in which the tag <root> is not closed"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/stage_1_no_closed_root.xml"), "Input XML file bad format: object <root> must be closed");
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/stage_1_no_closed_root.xml"), 
+            "Input XML file bad format: object <root> must be closed");
     }
     
     // Case of XML file with no errors --> we need to build a CBT that allows as to cover all the statements still uncovered
     WHEN("The XML file contains no errors"){
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/complete_CBT.xml"));
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_1/inputs/complete_CBT.xml"));
         REQUIRE(cbtree.get_actions().size() == 4);
     }
 }

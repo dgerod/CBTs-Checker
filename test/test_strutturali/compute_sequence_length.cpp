d32885de32acfb3bb9ae6fa68da2630702e34a65
Diego Escudero <dgerod@xyz-lab.org.es>
2022-03-20 15:17:43 +0100

    Rename structural test.

--- test/test_strutturali/compute_sequence_length.cpp
+++ test/test_strutturali/compute_sequence_length.cpp
@@ -16,7 +16,7 @@ SCENARIO("Test of the second stage of the pipeline: compute sequence length for 
     // In this case if we take the CBT with every type of node
     WHEN("completed the computations"){
         cbtc::conditioned_behavior_tree cbtree;            
-        cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_2/complete_CBT.xml");
+        cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/test_strutturali/stage_2/inputs/complete_CBT.xml");
         REQUIRE_NOTHROW(cbtree.compute_length_sequences());
         REQUIRE(cbtree.get_max_length_sequence() == 2);
     }

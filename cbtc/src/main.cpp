92b14453dcf21512c75bf3bdf26f35d5ea6e946f
Diego Escudero <dgerod@xyz-lab.org.es>
2022-03-26 10:32:48 +0100

    New argument to select temporary directory where validator store internal files.

--- cbtc/src/main.cpp
+++ cbtc/src/main.cpp
@@ -75,12 +75,13 @@ int main(int argc, const char * argv[])
 {
     simple_logger::enabled_level_ = simple_logger::level::INFO;
     
-    if (argc != 5)
+    if (argc != 6)
     {
         std::cerr << "Input Error: expected 4 inputs to the program\n";
         std::cout << "  configuration file" << std::endl;
         std::cout << "  tree" << std::endl;
         std::cout << "  initial state" << std::endl;
+        std::cout << "  temp directory" << std::endl;
         std::cout << "  output directory" << std::endl;
         
         return 1;
@@ -89,7 +90,8 @@ int main(int argc, const char * argv[])
     std::string limboole_app_path = read_configuration(argv[1]);
     std::string input_tree_file = argv[2];
     std::string input_requirements_file = argv[3]; 
-    std::string output_folder = argv[4];
+    std::string temp_folder = argv[4];
+    std::string output_folder = argv[5];
 
     std::cout << "Check if a Conditional Behavior Tree (CBT) could be executed" << std::endl;    
     std::cout << std::endl;
@@ -97,6 +99,7 @@ int main(int argc, const char * argv[])
     std::cout << "  - limboole path: " << limboole_app_path << std::endl;
     std::cout << "  - tree: " << input_tree_file << std::endl;
     std::cout << "  - initial state: " << input_requirements_file << std::endl;
+    std::cout << "  - temp directory: " << temp_folder << std::endl;
     std::cout << "  - output directory: " << output_folder << std::endl;
     std::cout << std::endl;
 
@@ -107,17 +110,17 @@ int main(int argc, const char * argv[])
         cbtc::conditioned_behavior_tree cbt;    
         cbtc::tree_loader::bt_from_yarp_xml(cbt, input_tree_file);
 
-        cbtc::cbt_validator cbt_validator(limboole_app_path, output_folder);    
+        cbtc::cbt_validator cbt_validator(limboole_app_path, temp_folder);    
         valid = cbt_validator.validate(cbt, input_requirements_file);
         write_result(output_folder, valid);
     }
     catch(const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     std::cout << std::endl;
     std::cout << "CBT is valid? " << (valid ? "YES" : "NO") << std::endl;
-    return 0;
+    return EXIT_SUCCESS;
 }

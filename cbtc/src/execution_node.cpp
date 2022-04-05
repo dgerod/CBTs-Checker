1463199a67c6a82e9b476721d03b26048cf82470
Diego Escudero <dgerod@xyz-lab.org.es>
2022-04-05 15:10:42 +0200

    Code fail when action label of the node is the last action in the set.

--- cbtc/src/execution_node.cpp
+++ cbtc/src/execution_node.cpp
@@ -58,7 +58,7 @@ int execution_node::set_ex_time(int current_time, task* parent)
     return current_time + 1;
 }
 
-int execution_node::get_plan(int current_time, task* parent, std::ofstream &file, std::set<action> * const actions)
+int execution_node::get_plan(int current_time, task* parent, std::ofstream &file, std::set<action>* const actions)
 {
     simple_logger(simple_logger::level::DEBUG) << "execution_node::get_plan" << std::endl;
     
@@ -66,22 +66,46 @@ int execution_node::get_plan(int current_time, task* parent, std::ofstream &file
     simple_logger(simple_logger::level::DEBUG) << "  - node type: " << node_type_to_string(this->type_) << std::endl;
     simple_logger(simple_logger::level::DEBUG) << "  - sequence length: " << this->sequence_length_ << std::endl;
     simple_logger(simple_logger::level::DEBUG) << "  - action label: " << this->action_label_ << std::endl;
+    simple_logger(simple_logger::level::DEBUG) << "  - num actions: " << actions->size() << std::endl;
+    simple_logger(simple_logger::level::DEBUG) << "  - parent type: " << parent->get_type() << std::endl;
 
     file << " & (eta_succ_" << this->get_identifier() << " -> " << "eta_try_" << this->get_identifier() << ")";
     file << " & (eta_succ_"<< this->get_identifier() << " <-> " << this->get_action_label() << "_" << this->get_ex_time() << ")";
 
     if (parent->get_type() != PARALLEL)
     {
-        file << " & (";
-        unsigned int j = 0;
+        simple_logger(simple_logger::level::DEBUG) << "all actions: ";
+        for (auto& a : *actions) {
+            simple_logger(simple_logger::level::DEBUG) << a.get_label() << ", ";
+        }
+        simple_logger(simple_logger::level::DEBUG) << std::endl;
+
+        std::set<action> filtered_actions;
         for(auto& a : *actions)
         {
-            if(a.get_label()!= this->get_action_label())
+            if(a.get_label() != this->get_action_label())
+            {  
+                filtered_actions.insert(a);
+            }            
+        }
+
+        simple_logger(simple_logger::level::DEBUG) << "filtered actions: ";
+        for (auto& a : filtered_actions) {
+            simple_logger(simple_logger::level::DEBUG) << a.get_label() << ", ";
+        }
+        simple_logger(simple_logger::level::DEBUG) << std::endl;
+
+        file << " & (";
+        unsigned int j = 0;
+        for(auto& a : filtered_actions)
+        {            
+            if (j < filtered_actions.size()-1)
+            {                
+                file << "!" << a.get_label() << "_" << this->get_ex_time() << " & ";
+            }
+            else
             {
-                if (j < actions->size()-1)
-                    file << "!" << a.get_label() << "_" << this->get_ex_time() << " & ";
-                else
-                    file << "!" << a.get_label() << "_" << this->get_ex_time();
+                file << "!" << a.get_label() << "_" << this->get_ex_time();
             }
             j++;
         }

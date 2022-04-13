fd1267e305022f0f9556e2239785b480f03178b0
Diego Escudero <dgerod@xyz-lab.org.es>
2022-04-13 16:35:02 +0200

    Add few valid cbt tests.

---
+++ test/check_bt_valid.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include "catch.hpp"
+
+#include "tree_loader.hpp"
+#include "cbt_validator.hpp"
+#include "conditioned_behavior_tree.hpp"
+
+
+std::string get_next_line(std::ifstream* const file)
+{
+    std::string line = "";
+    bool end_file = false;
+
+    while(line.length() == 0 and !end_file)
+    {
+        if(!file->eof()) 
+        {
+            std::getline(*file >> std::ws,line);
+        }
+        else
+        {
+            end_file = true;
+        }
+    }
+    return line;
+}
+
+const std::string read_configuration(const std::string file_path)
+{
+    std::ifstream file;     
+    file.open(file_path);       
+   
+    std::string line;
+    line = get_next_line(&file);
+    std::string limboole_path = line;
+
+    file.close();
+    return limboole_path;
+}
+
+const std::string CONFIG_FILE_PATH = "./test/config.txt";
+const std::string LIMBOOLE_PATH = read_configuration(CONFIG_FILE_PATH);
+const std::string TMP_DIRECTORY = "./test/_temp_";
+
+SCENARIO("BT is valid")
+{    
+    WHEN("Tree 1")
+    {
+        std::string tree_file = "./test/data/check_bt_valid/valid/tree_1.xml";
+        std::string init_state_file = "./test/data/check_bt_valid/no_valid/init_state.txt";
+        
+        cbtc::conditioned_behavior_tree cbtree;       
+        cbtc::tree_loader::bt_from_yarp_xml(cbtree, tree_file);
+        
+        THEN("valid")
+        {
+            cbtc::cbt_validator validator(LIMBOOLE_PATH, TMP_DIRECTORY);            
+            REQUIRE(validator.validate(cbtree, init_state_file));
+        }
+    }
+}
+
+SCENARIO("BT is NOT valid")
+{
+    WHEN("Tree 1")
+    {
+        std::string tree_file = "./test/data/check_bt_valid/no_valid/tree_1.xml";
+        std::string init_state_file = "./test/data/check_bt_valid/no_valid/init_state.txt";
+
+        cbtc::conditioned_behavior_tree cbtree;       
+        cbtc::tree_loader::bt_from_yarp_xml(cbtree, tree_file);
+        
+        THEN("NOT valid")
+        {
+            cbtc::cbt_validator validator(LIMBOOLE_PATH, TMP_DIRECTORY);            
+            REQUIRE_FALSE(validator.validate(cbtree, init_state_file));
+        }
+    }
+   
+  
+}
--- test/load_bt_from_xml.cpp
+++ test/load_bt_from_xml.cpp
@@ -40,6 +40,120 @@ SCENARIO("Error loading BT from YARP XML file")
         REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/tree_4.xml"), 
             "Tree in input xml file is not well formated.");
     }
+
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f1_1.xml"), 
+            "Input XML file bad format: Behavior Tree must have a root");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f1_2.xml"), 
+            "Input XML file bad format: root must be followed by <BehaviorTree>");
+    }
+
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f1_3.xml"), 
+            "Input XML file bad format: Behavior Tree bad format");
+    }
+
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f2_1.xml"), 
+            "Tree in input xml file is not well formated.");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f2_2.xml"), 
+            "Tree in input xml file is not well formated.");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f2_3.xml"), 
+            "Tree in input xml file is not well formated.");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f3_1.xml"), 
+            "Input XML file bad format: after \"not \" Pre and Post conditions cannot contain whitespaces");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f3_2.xml"), 
+            "Input XML file bad format: positive Pre and Post conditions cannot contain whitespaces");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f3_3.xml"), 
+            "Exception occurred while opening the file: input xml file not found.");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f4_1.xml"), 
+            "Action not defined in the vocabulary");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f4_2.xml"), 
+            "Action not defined in the vocabulary");
+    }
+
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f5.xml"), 
+            "Tree in input xml file is not well formated.");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f6.xml"), 
+            "Input XML file bad format: action cannot have 'c' and 'not c' as pre-conditions");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f7.xml"), 
+            "Input XML file bad format: action cannot have 'c' and 'not c' as post-conditions");
+    }
+    
+    /*
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f8.xml"), 
+            "Tree in input xml file is not well formated.");
+    }
+    
+    WHEN("The XML file is malformed")
+    {
+        cbtc::conditioned_behavior_tree cbtree;            
+        REQUIRE_THROWS_WITH(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/wrong_format/req_f9.xml"), 
+            "Tree in input xml file is not well formated.");
+    }
+    */    
 }
 
 SCENARIO("Load BT from YARP XML file"){
@@ -47,48 +161,48 @@ SCENARIO("Load BT from YARP XML file"){
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_1.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_1.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_2.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_2.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_3.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_3.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_4.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_4.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_5.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_5.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_6.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_6.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/tree_7.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/tree_7.xml"));        
     }
     
     WHEN("The XML file contains no errors")
     {
         cbtc::conditioned_behavior_tree cbtree;            
-        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/valid_format/complete_CBT.xml"));        
+        REQUIRE_NOTHROW(cbtc::tree_loader::bt_from_yarp_xml(cbtree, "./test/data/load_bt_from_xml/inputs/correct_format/complete_CBT.xml"));        
     }    
 }
